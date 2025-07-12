a85474044e1e8f23f6f324db62309681c1786e28
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-07-12 22:52:22 +0500

    Time: 2 ms (64.28%) | Memory: 57.7 MB (99.41%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+    let count1 = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            count++;
+        } else if (nums[i] !== 1) {
+            if (count > count1) {
+            count1 = count;
+        }
+      count = 0;
+    }
+  }
+  return Math.max(count1, count);
+};
