24f5006faaca86a4cdcb5cb065c3ec5f3bca10e1
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-05 11:02:04 +0500

    Time: 9 ms (42.04%) | Memory: 77 MB (62.07%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -10,25 +10,32 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    
-    function toArray(){
-        let arr = [];
-        let curr = head;
+  // finding the middle
+  let slow = fast = head;
+  while(fast && fast.next){
+    slow = slow.next;
+    fast = fast.next.next;
+  }  
 
-        while (curr != null){
-            arr.push(curr.val);
-            curr = curr.next;
-        }
-        return arr;
+  // reverse the second half of list
+  let prev = null;
+  let curr = slow;
+  while(curr){
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+
+  // finding the palindrome
+  let firstList = head;
+  let secondList = prev;
+  while (secondList){
+    if (firstList.val != secondList.val){
+        return false;
     }
-    let arr = toArray();
-
-    let n = arr.length;
-    for (let i = 0; i < Math.floor(n/2); i++){
-        if (arr[i] != arr[n - i -1]) {
-            return false;
-        }
-    }
-    return true;
-
-}
+    firstList = firstList.next;
+    secondList = secondList.next;
+  }
+  return true;
+};
