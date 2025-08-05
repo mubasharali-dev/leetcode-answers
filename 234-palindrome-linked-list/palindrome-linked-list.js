5af2deffe4646b30790ea5e3190e6f6db4fd6a0d
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-05 11:05:26 +0500

    Update palindrome-linked-list.js

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -9,6 +9,32 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// Approach 1:
+var isPalindrome = function(head) {
+    function toArray(){
+        let arr = [];
+        let curr = head;
+
+        while (curr != null){
+            arr.push(curr.val);
+            curr = curr.next;
+        }
+        return arr;
+    }
+
+    let arr = toArray();
+
+    let n = arr.length;
+    for (let i =0; i < Math.floor(n/2); i++ ){
+        if (arr[i] !== arr[n-i-1]){
+            return false;
+        }
+    }
+    return true;
+};
+
+// Approach 2:
+
 var isPalindrome = function(head) {
   // finding the middle
   let slow = fast = head;
