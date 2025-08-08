16bb5547af0078f635df3da3168ea59a9803705a
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-08 20:52:50 +0500

    Update remove-nth-node-from-end-of-list.js

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -10,6 +10,33 @@
  * @param {number} n
  * @return {ListNode}
  */
+
+// --------------- Approach 1 - Two pass ----------------------
+
+var removeNthFromEnd = function(head, n) {
+    let sentinel = new ListNode();
+    sentinel.next = head;
+    let length = 0;
+
+    while (head){
+        head = head.next;
+        length++;
+    }
+
+    let prevPos = length - n;
+
+    let prev = sentinel;
+
+    for (let i =0; i< prevPos; i++){
+        prev = prev.next;
+    }
+    prev.next = prev.next.next;
+
+    return sentinel.next;
+};
+
+
+// --------------- Approach 2 - One pass ----------------------
 var removeNthFromEnd = function(head, n) {
     let sentinel = new ListNode();
     sentinel.next = head;
