5d98b045f98e1713189580a1b5e4e7ca1df8ee57
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-12 16:36:14 +0500

    Update swap-nodes-in-pairs.js

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -9,6 +9,32 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+// Approach 1 - Iterative solution
+
+var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+    
+    let dummy = new ListNode();
+    dummy.next = head;
+
+    let p = dummy;
+    let c = head;
+    let n = head.next;
+
+    while (c && n){
+        p.next = n;
+        c.next = n.next;
+        n.next = c;
+
+        p = c;
+        c = p.next;
+        n = c && c.next;
+    }
+    return dummy.next;
+};
+
+// Approach 2 - Recursive Solution
 var swapPairs = function(head) {
     if (!head || !head.next) return head;
 
