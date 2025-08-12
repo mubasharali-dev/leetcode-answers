a72e9a77fc6b206e1687cce5fbc8e2d94592f76f
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-12 16:33:36 +0500

    Time: 0 ms (100.00%) | Memory: 53.8 MB (30.57%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -11,22 +11,12 @@
  */
 var swapPairs = function(head) {
     if (!head || !head.next) return head;
+
+    let l = head;
+    let r = head.next;
+
+    l.next = swapPairs(r.next);
+    r.next = l;
     
-    let dummy = new ListNode();
-    dummy.next = head;
-
-    let p = dummy;
-    let c = head;
-    let n = head.next;
-
-    while (c && n){
-        p.next = n;
-        c.next = n.next;
-        n.next = c;
-
-        p = c;
-        c = p.next;
-        n = c && c.next;
-    }
-    return dummy.next;
+    return r;
 };
