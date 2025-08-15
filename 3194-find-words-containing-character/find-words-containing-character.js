b86443c3d6095b88b88910dffd0f37953311ea66
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-15 16:19:01 +0500

    Time: 1 ms (86.65%) | Memory: 56 MB (99.18%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -3,12 +3,28 @@
  * @param {character} x
  * @return {number[]}
  */
+// Approach 1 - Using built in function (includes)
+// var findWordsContaining = function(words, x) {
+//     let indices = [];
+//     for (let i =0; i < words.length; i++){
+//         if (words[i].includes(x)){
+//             indices.push(i);
+//         }
+//     }
+//     return indices;
+// };
+
+// Approach 2 - Reommended
+
 var findWordsContaining = function(words, x) {
     let indices = [];
     for (let i =0; i < words.length; i++){
-        if (words[i].includes(x)){
-            indices.push(i);
-        } 
+        for (let j =0; j < words[i].length; j++){
+            if (words[i][j] == x ){
+                indices.push(i);
+                break;
+            }
+        }
     }
     return indices;
 };
