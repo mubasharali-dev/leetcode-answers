b780cc90a8e339186fd106df67185508204be57c
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-16 13:47:45 +0500

    Time: 1 ms (49.72%) | Memory: 54 MB (50.53%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -3,12 +3,28 @@
  * @param {string} stones
  * @return {number}
  */
+
+// var numJewelsInStones = function(jewels, stones) {
+//     let count = 0;
+
+//     for (let i =0; i < stones.length; i++){
+//         if (jewels.includes(stones[i])){
+//             count++;
+//         }
+//     }
+//     return count;
+// };
+
+
 var numJewelsInStones = function(jewels, stones) {
     let count = 0;
 
-    for (let i =0; i < stones.length; i++){
-        if (jewels.includes(stones[i])){
-            count++;
+    for (let i = 0; i < stones.length; i++){
+        for (let j = 0; j < jewels.length; j++){
+            if (jewels[j] == stones[i]){
+                count++;
+                break;
+            }
         }
     }
     return count;
