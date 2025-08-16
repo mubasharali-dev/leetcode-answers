dcb3dd144ecebc6aa462ee812936c5cb77436be5
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-16 13:52:37 +0500

    Time: 1 ms (49.72%) | Memory: 54 MB (50.53%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -4,6 +4,7 @@
  * @return {number}
  */
 
+// APPROACH 1 - USING THE BUILT IN FUNCTION (INCLUDES)
 // var numJewelsInStones = function(jewels, stones) {
 //     let count = 0;
 
@@ -16,15 +17,33 @@
 // };
 
 
+// APPROACH 2 - LANGUAGE AGNOSTIC BUT TC => O(n^2)
+// var numJewelsInStones = function(jewels, stones) {
+//     let count = 0;
+
+//     for (let i = 0; i < stones.length; i++){
+//         for (let j = 0; j < jewels.length; j++){
+//             if (jewels[j] == stones[i]){
+//                 count++;
+//                 break;
+//             }
+//         }
+//     }
+//     return count;
+// };
+
+// APPROACH 3 - OPTIMIZED AND RECOMMENDED
 var numJewelsInStones = function(jewels, stones) {
     let count = 0;
+    let jSet = new Set();
+
+    for (let i = 0; i < jewels.length; i++){
+        jSet.add(jewels[i]);
+    }
 
     for (let i = 0; i < stones.length; i++){
-        for (let j = 0; j < jewels.length; j++){
-            if (jewels[j] == stones[i]){
-                count++;
-                break;
-            }
+        if (jSet.has(stones[i])){
+            count++;
         }
     }
     return count;
