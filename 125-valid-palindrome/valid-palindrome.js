0d849cea945340b3b4dead5fdd7a3b3be5dc6278
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-20 19:31:49 +0500

    Time: 11 ms (16.61%) | Memory: 64.5 MB (5.75%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,16 +2,31 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
+ // APPROACH 1 --- USING THE BUILT IN FUNCTIONS
+ var isPalindrome = function(s) {
   s = s.toLowerCase();
   let filtered = "";
-  let reversed = "";
 
   for (let i =0; i < s.length; i++){
     if (s[i].match(/[a-z0-9]/i)){
         filtered += s[i];
-        reversed = s[i] + reversed;
     }
   }
+  let reversed = filtered.split("").reverse().join("");
   return filtered === reversed;
 };
+
+// APPROACH 2 - BUT WITH EXTRA SPACE
+// var isPalindrome = function(s) {
+//   s = s.toLowerCase();
+//   let filtered = "";
+//   let reversed = "";
+
+//   for (let i =0; i < s.length; i++){
+//     if (s[i].match(/[a-z0-9]/i)){
+//         filtered += s[i];
+//         reversed = s[i] + reversed;
+//     }
+//   }
+//   return filtered === reversed;
+// };
