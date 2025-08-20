85b2e60f0298e7b06adab8cf3628214d053b892d
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-08-20 20:56:34 +0500

    Time: 5 ms (64.42%) | Memory: 60.7 MB (24.02%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,18 +3,18 @@
  * @return {boolean}
  */
  // APPROACH 1 --- USING THE BUILT IN FUNCTIONS
- var isPalindrome = function(s) {
-  s = s.toLowerCase();
-  let filtered = "";
+//  var isPalindrome = function(s) {
+//   s = s.toLowerCase();
+//   let filtered = "";
 
-  for (let i =0; i < s.length; i++){
-    if (s[i].match(/[a-z0-9]/i)){
-        filtered += s[i];
-    }
-  }
-  let reversed = filtered.split("").reverse().join("");
-  return filtered === reversed;
-};
+//   for (let i =0; i < s.length; i++){
+//     if (s[i].match(/[a-z0-9]/i)){
+//         filtered += s[i];
+//     }
+//   }
+//   let reversed = filtered.split("").reverse().join("");
+//   return filtered === reversed;
+// };
 
 // APPROACH 2 - BUT WITH EXTRA SPACE
 // var isPalindrome = function(s) {
@@ -30,3 +30,24 @@
 //   }
 //   return filtered === reversed;
 // };
+
+// APPROACH 3 - WITHOUT USING EXTRA SPACE
+ var isPalindrome = function(s) {
+    s = s.toLowerCase();
+    let i = 0;
+    let j = s.length - 1;
+
+    while (i<j){
+        if (!s[i].match(/[a-z0-9]/i)){
+            i++;
+        } else if (!s[j].match(/[a-z0-9]/i)){
+            j--;
+        } else if (s[i] === s[j]){
+            i++;
+            j--;
+        } else {
+            return false;
+        }
+    }
+    return true;
+};
