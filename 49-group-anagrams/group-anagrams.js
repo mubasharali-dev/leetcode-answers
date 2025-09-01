4407c2e3127d15a2144b5da29208c582379d500b
Mubashar Ali <102550281+mubasharali-dev@users.noreply.github.com>
2025-09-01 16:30:04 +0500

    Time: 38 ms (37.87%) | Memory: 68.5 MB (33.91%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,15 +2,45 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+
+// ---------------- APPROACH 1 ---------------- 
+// var groupAnagrams = function(strs) {
+//     let map = {};
+//     for (let i = 0; i < strs.length; i++){
+//         let sortedStr = strs[i].split("").sort().join("");
+//         if (!map[sortedStr]){
+//             map[sortedStr] = [strs[i]];
+//         } else {
+//             map[sortedStr].push(strs[i]);
+//         }
+//     }
+//     return [...Object.values(map)];
+// };
+
+// --------------- APPROACH 2 - OPTIMIZED FOR TIME COMPLEXITY - O(n * m)
 var groupAnagrams = function(strs) {
     let map = {};
     for (let i = 0; i < strs.length; i++){
-        let sortedStr = strs[i].split("").sort().join("");
-        if (!map[sortedStr]){
-            map[sortedStr] = [strs[i]];
-        } else {
-            map[sortedStr].push(strs[i]);
+        // create the array for mapping the characters
+        let freqArr = Array(26).fill(0);
+        let s = strs[i];
+        // fill the empty character array with alphabets
+        for (let j =0; j < s.length; j++){
+            let index = s[j].charCodeAt() - "a".charCodeAt();
+            ++freqArr[index];
         }
-    }
+        // key to track the characters
+        let key = "";
+        for (let k = 0; k < 26; k++){
+            key = key + String.fromCharCode(k) + freqArr[k];
+        }
+
+        // increment the key or push it
+        if (!map[key]){
+            map[key] = [s];
+        } else {
+            map[key].push(s);
+        }
+}
     return [...Object.values(map)];
 };
